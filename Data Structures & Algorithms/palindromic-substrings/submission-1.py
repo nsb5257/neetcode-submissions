dde67732671c4d36835b00ec8ffe7aee9b5ea530
THADIKONDA NAGA SAI BALAJI <138895938+nsb5257@users.noreply.github.com>
2026-08-27 00:56:30 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        cnt = 0
+        def finpalindrome(i):
+            count = 0
+            l = i
+            r = i
+            while l>=0 and r<n and s[l]==s[r]:
+                count += 1
+                l-=1
+                r+=1
+            l=i
+            r=i+1
+            while l>=0 and r<n and s[l]==s[r]:
+                count+=1
+                l-=1
+                r+=1
+            return count
+
+        for c in range(0,n):
+            cnt += finpalindrome(c)
+        return cnt
