4d811209dd6b8d83d5a2366591981d5dffaa157c
THADIKONDA NAGA SAI BALAJI <138895938+nsb5257@users.noreply.github.com>
2026-08-31 22:26:32 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dummy=""
+        dp={}
+        def f(i,d):
+            if i >=n:
+                return True if d == "" else False
+            if (i+1,d) in dp:
+                return dp[(i+1,d)]
+            d += s[i]
+            if d in wordDict:
+                clear = f(i+1,"")
+                notclear = f(i+1,d)
+                dp[(i,d)] = clear or notclear
+                return dp[(i,d)]
+            dp[(i+1,d)] = f(i+1,d)
+            return dp[(i+1,d)]
+        
+        return f(0,"")
