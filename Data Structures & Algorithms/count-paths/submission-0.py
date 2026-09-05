d85ed6e86281aaf8a546592a72306e5123099b0d
THADIKONDA NAGA SAI BALAJI <138895938+nsb5257@users.noreply.github.com>
2026-09-05 11:11:15 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = {}
+        def f(i,j):
+            if i < 0 or j < 0:
+                return 0
+            if i == 0 and j==0:
+                return 1
+            if (i,j) in dp:
+                return dp[(i,j)]
+            dp[(i,j)] = f(i-1,j)+f(i,j-1)
+            return dp[(i,j)]
+        return f(m-1,n-1)
+
+        
